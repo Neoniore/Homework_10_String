a46442d08ce8a91cc69c429f24b7779d37b14b95
TabulaRasa <lforjust@gmail.com>
2023-10-10 17:22:08 +0300

    hw_10 is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        String firstName = "Ivanov", middleName = "Ivan", lastName = "Ivanovich", fullName;
+        fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+
+        //Задание 2
+        System.out.println("Задание 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println();
+
+        //Задание 3
+        System.out.println("Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
     }
 }
